d8f7a3e6c3c00aae6eec6029f99b2b16c48bee2b
Luís Correia <luis.mcorreia97@gmail.com>
2017-05-21 20:05:58 +0100

    já roda melhor e surpresa se carregares 'f' sem haver targets

---
+++ LightingScene.js
@@ -0,0 +1,364 @@
+var degToRad = Math.PI / 180.0;
+
+var BOARD_WIDTH = 6.0;
+var BOARD_HEIGHT = 4.0;
+
+var BOARD_A_DIVISIONS = 100;
+var BOARD_B_DIVISIONS = 100;
+
+var speed;
+var currTime, deltaTime;
+var light0, light1, run;
+
+function LightingScene() {
+	CGFscene.call(this);
+	currentTime = Date.now();
+  	
+}
+
+LightingScene.prototype = Object.create(CGFscene.prototype);
+LightingScene.prototype.constructor = LightingScene;
+
+LightingScene.prototype.init = function(application) {
+	CGFscene.prototype.init.call(this, application);
+	this.initCameras();
+	this.light0=true; this.light1=true; this.run = true; this.speed=3;
+	this.launch = false;
+	this.hit = false;
+	this.end = false;
+
+	this.initLights();
+	this.enableTextures(true);
+
+	this.gl.clearColor(0.0, 0.4, 0.7, 1.0);
+	this.gl.clearDepth(100.0);
+	this.gl.enable(this.gl.DEPTH_TEST);
+	this.gl.enable(this.gl.CULL_FACE);
+	this.gl.depthFunc(this.gl.LEQUAL);
+	this.gl.enable(this.gl.BLEND);
+    this.gl.blendFunc(this.gl.SRC_ALPHA, this.gl.ONE_MINUS_SRC_ALPHA);
+
+	this.axis = new CGFaxis(this);
+	this.submarine = new MySubmarine(this);
+	this.cleck = new MyClock(this, 90, 180,270);
+	this.post = new MyCylinder(this, 20,20);
+	this.materialDefault = new CGFappearance(this);
+	this.oceanFloor = new MyQuad(this, -0.5,1.5,-1,1);
+	this.trapeze = new MyTrapeze(this);
+	this.target = new MyTarget(this);
+	this.torpedos = [new MyTorpedo(this)];
+	this.target1 = new MyTarget(this, 5, -1, 10);
+	this.target2 = new MyTarget(this, -6, -3, 13);
+	this.targets = [this.target2, this.target1];
+	this.targetX = this.targets[0].x;
+	this.targetY = this.targets[0].y;
+	this.targetZ = this.targets[0].z;
+	this.activeTarget = 0;
+	this.infoBoard = new MyQuad(this);
+	// Explosion
+
+	this.explosionAppearance = new CGFappearance(this);
+	this.explosionAppearance.setAmbient(0.3,0.3,0.3,1);
+	this.explosionAppearance.setDiffuse(0.6,0.6,0.6,1);
+	this.explosionAppearance.setSpecular(0.8,0.8,0.8,1);	
+	this.explosionAppearance.setShininess(120);
+	this.explosionAppearance.loadTexture("../resources/images/explosion_teste2.png");
+	this.explosionAppearance.setTextureWrap("CLAMP_TO_EDGE", "CLAMP_TO_EDGE");
+
+
+	this.hand2Appearance = new CGFappearance(this);
+	this.hand2Appearance.setAmbient(0.3,0.3,0.3,1);
+	this.hand2Appearance.setDiffuse(1,0,0,1);
+	this.hand2Appearance.setSpecular(0.8,0.8,0.8,1);	
+	this.hand2Appearance.setShininess(120);
+		
+	this.reckAppearance = new CGFappearance(this);
+	this.reckAppearance.setAmbient(0.3,0.3,0.3,1);
+	this.reckAppearance.setDiffuse(0.6,0.6,0.6,1);
+	this.reckAppearance.setSpecular(0.8,0.8,0.8,1);	
+	this.reckAppearance.setShininess(120);
+	this.reckAppearance.loadTexture("../resources/images/reck.png");
+	this.reckAppearance.setTextureWrap("CLAMP_TO_EDGE", "CLAMP_TO_EDGE");
+
+	this.handAppearance = new CGFappearance(this);
+	this.handAppearance.setAmbient(0.3,0.3,0.3,1);
+	this.handAppearance.setDiffuse(0,0,0,1);
+	this.handAppearance.setSpecular(0.8,0.8,0.8,1);	
+	this.handAppearance.setShininess(120);
+	
+	this.cleckAppearance = new CGFappearance(this);
+	this.cleckAppearance.setAmbient(0.3,0.3,0.3,1);
+	this.cleckAppearance.setDiffuse(0.6,0.6,0.6,1);
+	this.cleckAppearance.setSpecular(0.8,0.8,0.8,1);	
+	this.cleckAppearance.setShininess(120);
+	this.cleckAppearance.loadTexture("../resources/images/cleck.png");
+	this.cleckAppearance.setTextureWrap("CLAMP_TO_EDGE", "CLAMP_TO_EDGE");
+
+	this.oceanAppearance = new CGFappearance(this);
+	this.oceanAppearance.setAmbient(0.7,0.7,0.7,1);
+	this.oceanAppearance.setDiffuse(0.7,1,0.7,1);
+	this.oceanAppearance.setSpecular(0.8,0.8,0.8,1);	
+	this.oceanAppearance.setShininess(120);
+	this.oceanAppearance.loadTexture("../resources/images/ocean.jpg");
+	this.oceanAppearance.setTextureWrap("MIRRORED_REPEAT", "MIRORRED_REPEAT");
+	
+	this.infoAppearance = new CGFappearance(this);
+	this.infoAppearance.setAmbient(0.3,0.3,0.3,1);
+	this.infoAppearance.setDiffuse(0.6,0.6,0.6,1);
+	this.infoAppearance.setSpecular(0.8,0.8,0.8,1);	
+	this.infoAppearance.setShininess(120);
+	this.infoAppearance.loadTexture("../resources/images/info.png");
+	this.infoAppearance.setTextureWrap("CLAMP_TO_EDGE", "CLAMP_TO_EDGE");
+
+
+	this.setUpdatePeriod(1);
+	
+	this.currSubmarineAppearance = 0;
+	this.submarineAppearanceList = {'steel': 0, 'artsy': 1, 'cage': 2, 'poolball': 3};
+	
+};
+
+
+LightingScene.prototype.initCameras = function() {
+	this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(30, 30, 30), vec3.fromValues(0, 0, 0));
+};
+
+LightingScene.prototype.initLights = function() {
+	this.setGlobalAmbientLight(0.7,0.7,0.7, 1.0);
+	
+	// Positions for four lights
+	//light 0
+	this.lights[0].setPosition(4, 6, 3, 1);
+	this.lights[0].setVisible(true); // show marker on light position (different from enabled)
+	
+	this.lights[0].setAmbient(0.5, 0.5, 0.5, 1);
+	this.lights[0].setDiffuse(0.7, 0.7,0.7, 1.0);
+	this.lights[0].setConstantAttenuation(0);
+	this.lights[0].setLinearAttenuation(0.15);
+	this.lights[0].setQuadraticAttenuation(0);
+	this.lights[0].enable();
+	
+	//light 1
+	this.lights[1].setPosition(-4, 6, 3, 1);
+	this.lights[1].setVisible(true); // show marker on light position (different from enabled)
+	
+	this.lights[1].setAmbient(0.5, 0.5, 0.5, 1);
+	this.lights[1].setDiffuse(0.7, 0.7,0.7, 1.0);
+	this.lights[1].setConstantAttenuation(0);
+	this.lights[1].setLinearAttenuation(0.15);
+	this.lights[1].setQuadraticAttenuation(0);
+	this.lights[1].enable();
+	
+};
+
+LightingScene.prototype.updateLights = function() {
+	
+	if(this.light0 == true)
+		this.lights[0].enable();
+	else if(this.light0 == false)
+		this.lights[0].disable();
+	
+	if(this.light1 == true)
+		this.lights[1].enable();
+	else if(this.light1 == false)
+		this.lights[1].disable();	
+
+
+	for (i = 0; i < this.lights.length; i++){
+		this.lights[i].update();
+	}
+		
+}
+
+LightingScene.prototype.display = function() {
+	// ---- BEGIN Background, camera and axis setup
+
+	// Clear image and depth buffer everytime we update the scene
+	this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+	this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+
+	// Initialize Model-View matrix as identity (no transformation)
+	this.updateProjectionMatrix();
+	this.loadIdentity();
+
+	// Apply transformations corresponding to the camera position relative to the origin
+	this.applyViewMatrix();
+
+	this.materialDefault.apply();
+	// Draw axis
+	this.axis.display();
+
+	// Update all lights used
+	this.updateLights();
+	
+	this.scale(2,2,2);
+	
+	this.pushMatrix(); 
+		this.reckAppearance.apply();
+		this.rotate(-90 * degToRad, 1, 0, 0);
+		this.scale(1, 1, 8);
+	this.popMatrix();
+
+	this.pushMatrix();
+		this.cleckAppearance.apply();
+		this.scale(0.7,0.7,1)
+		this.translate(8/0.7,5/0.7,0);
+		this.cleck.display();
+	this.popMatrix();
+
+	this.pushMatrix();
+		this.translate(8,0,0);
+		this.scale(0.1,5,0.1);
+		this.rotate(Math.PI /2, -1,0,0);
+		this.reckAppearance.apply();
+		this.post.display();
+	this.popMatrix();
+	
+	this.pushMatrix();
+		this.oceanAppearance.apply();
+		this.translate(0,-4,0);
+		this.scale(50,1,50);
+		this.rotate(Math.PI /2, -1,0,0);
+		this.oceanFloor.display();
+	this.popMatrix();
+
+	//Targets
+	for(var i=0; i<this.targets.length; i++){
+		this.pushMatrix();
+				this.targets[i].display();
+		this.popMatrix();
+	}
+
+	
+
+	//Sub
+
+	this.pushMatrix();
+		this.materialDefault.apply();
+		this.translate(this.submarine.posX,this.submarine.posY,
+		this.submarine.posZ);
+		this.submarine.swim();
+		var time = Date.now();
+		this.submarine.turbinesRotation(time);
+	
+		this.pushMatrix();
+		    this.submarine.display();
+		this.popMatrix();
+	
+		this.translate(0,0,5);
+		this.scale(3,.1,1);
+	this.popMatrix();
+	
+	
+
+	//Torpedo
+	
+	if(this.launch){
+		this.torpedos[0].readyToFire();
+		this.torpedos[0].display();
+	}
+	
+		
+
+
+	//Explosion
+	if (this.exploded) {
+		this.pushMatrix();
+			this.explosionAppearance.apply();
+			this.explosion.display();
+		this.popMatrix();
+	}
+
+	//Info Board
+	if(this.end){
+		 this.pushMatrix();
+	  		this.infoAppearance.apply();
+	  		this.rotate(Math.PI,0,1,0);
+	  		this.translate(0,0,-8);
+	  		this.scale(4,3,4);
+	  		this.infoBoard.display();
+	  	this.popMatrix();
+	}
+
+
+	
+
+	//this.torpedo = new MyTorpedo(this, 0,0,0,0,0);
+	//this.torpedo.display();
+	
+	//this.popMatrix();
+	
+	// ---- END Background, camera and axis setup
+	
+	// ---- BEGIN Geometric transformation section
+
+	// ---- END Geometric transformation section
+
+	// ---- BEGIN Primitive drawing section
+	
+	// ---- END Primitive drawing section
+};
+
+
+
+LightingScene.prototype.update = function(currentTime) {
+	this.removeTarget();
+	if(this.run){
+		var hourAngle = 30*(1+(currentTime/3600000)%(12));
+		var minAngle = 6*((currentTime/(60000))%(60));
+		var secAngle = 360*(((currentTime/1000)%60)/60);
+		MyClock(this,hourAngle, minAngle, secAngle);
+	}
+	//this.torpedo.updatePosition(this.submarine.x, this.submarine.y, this.submarine.z);
+	if(this.launch)
+		this.torpedos[0].update(currentTime);
+
+	//this.removeTarget();
+	
+};
+
+LightingScene.prototype.clock = function() {
+	this.run = !this.run;
+};
+
+LightingScene.prototype.fire = function() {
+	if(this.targets.length != 0){
+	this.launch = !this.launch;
+	this.torpedos[0] = new MyTorpedo(this, this.submarine.posX, 
+	 this.submarine.posY-.8, this.submarine.posZ, 
+	  this.submarine.rotY, this.submarine.currVertRot);
+	  }
+	  else{
+		this.end = true;
+	  }
+	  	
+
+};
+
+
+LightingScene.prototype.removeTarget = function(){
+	//remove first element
+	if (this.hit){
+		this.targets.shift();
+		this.torpedos.pop();
+		this.torpedos.push(new MyTorpedo(this));
+		
+	
+		this.hit = false;
+	}
+
+}
+
+
+
+LightingScene.prototype.explode = function(x,y,z) {
+	this.explosion = new MyExplosion(this,x,y,z);
+	this.exploded = true;
+
+}
+
+
+
+
+
+
---
+++ MyTorpedo.js
@@ -0,0 +1,313 @@
+/**
+ * MyTorpedo
+ * @constructor
+ */
+
+ function MyTorpedo(scene, x, y, z, hAngle, vAngle) {
+ 	this.cylinder = new MyCylinder(scene, 20,20);
+	this.sphere = new MySphere(scene,20,20);
+ 	CGFobject.call(this,scene);
+	this.triangle = new MyTriangle(scene);
+	this.circle = new MyCircle(scene,20,20);
+	this.innerCylinder = new MyInsideCylinder(scene,20,20);
+	this.trapeze = new MyTrapeze(scene);
+	this.quad = new MyQuad(scene);
+	
+	this.steelAppearance = new CGFappearance(this.scene);
+	this.steelAppearance.loadTexture("../resources/images/steel.jpg");
+	
+	this.hasExploded = false;
+	this.x = x;
+	this.y = y;
+	this.z = z -1.5 ;
+
+	//this.angle1 = angle;
+	//this.angle2 = 0;
+	this.orientation = 0;
+	this.hRot = hAngle;
+	this.vRot = vAngle;
+
+	this.initialHRot = hAngle;
+	this.initialVRot = vAngle;
+
+	//this.P2;
+	//this.P3;	
+	this.P4;
+	this.t = 0;
+	
+	//this.submarineStopped = false;
+
+	//this.rotation = false;
+	this.launch = false;
+	this.timePassed;
+
+	this.firingTime;
+
+	this.y_offset = -1.6;
+	this.z_offset = 1.5;
+	
+	
+	
+
+};
+
+ MyTorpedo.prototype = Object.create(CGFobject.prototype);
+ MyTorpedo.prototype.constructor = MyTorpedo;
+
+ MyTorpedo.prototype.display = function() {
+ 	this.steelAppearance.apply();
+ 	// Body
+ 	//this.scene.rotate(this.currVertRot * degToRad,1,0,0);
+ 	//this.scene.translate(0,0,-2);
+ 	this.scene.translate(this.x, this.y, this.z+0.5);
+	this.scene.rotate(this.hRot,0,1,0);
+	this.scene.rotate(-this.vRot, 1, 0, 0);
+	this.scene.translate(0,0,-2);
+
+ 	this.scene.pushMatrix();
+ 		this.scene.scale(.2,.2,2);
+ 		this.cylinder.display();
+ 	this.scene.popMatrix();
+ 	
+ 	this.scene.pushMatrix();
+ 		this.scene.scale(.2,.2,.2);
+ 		this.scene.rotate(Math.PI,0,0,1);
+ 		this.scene.translate(0,0,10);
+ 		this.sphere.display();
+ 	this.scene.popMatrix();	
+ 	this.scene.pushMatrix();
+ 		this.scene.scale(.2,.2,.2);
+ 		this.scene.rotate(Math.PI, 1,0,0);
+ 		this.sphere.display();
+ 	this.scene.popMatrix();	
+ 	
+
+ 	//Fins
+
+	this.scene.pushMatrix();
+	//Left
+		this.scene.pushMatrix();
+			this.scene.translate(.3,0,0);
+			this.scene.scale(.5,0.05,.15);
+			this.trapeze.display();
+		this.scene.popMatrix();
+	//Right
+		this.scene.pushMatrix();
+			this.scene.rotate(Math.PI,0,0,1);
+			this.scene.translate(.3,0,0);
+			this.scene.scale(.5,0.05,.15);
+			this.scene.translate(0,-0.5,0);
+			this.trapeze.display();
+		this.scene.popMatrix();
+	//Up
+		this.scene.pushMatrix();
+			this.scene.rotate(Math.PI/2,0,0,1);
+			this.scene.translate(.3,0,0);
+			this.scene.scale(.5,0.05,.15);
+			
+			//this.scene.translate(0,0,0);
+			this.trapeze.display();
+		this.scene.popMatrix();
+	//Down
+		this.scene.pushMatrix();
+			this.scene.rotate(-Math.PI/2,0,0,1);
+			this.scene.translate(.3,0,0);
+			this.scene.scale(.5,0.05,.15);
+			this.scene.translate(0,-0.5,0);
+			this.trapeze.display();
+		this.scene.popMatrix();
+	this.scene.popMatrix();
+
+	
+
+ };
+	
+ 
+ MyTorpedo.prototype.updatePosition = function(x, y, z){
+
+        this.x = this.scene.submarine.x;
+        this.y = this.scene.submarine.y - 0.8;
+        this.z = this.scene.submarine.z;
+ };
+
+
+ MyTorpedo.prototype.getVector = function(){
+
+ 	this.P4 = [
+		this.scene.targets[0].x,
+		this.scene.targets[0].y,
+		this.scene.targets[0].z,
+ 	];
+
+ 	//console.log(this.P4[0] + " " + this.P4[1]+ "  " + this.P4[2]);
+
+ 	var vector = [
+		this.P4[0]-this.x,
+		this.P4[1]-this.y,
+		this.P4[2]-this.z,	
+ 	];
+
+ 	return vector;
+
+
+ };
+
+ MyTorpedo.prototype.getTargetDistance = function(){
+ 	var vector = this.getVector();
+ 	var x =Math.sqrt(Math.pow(vector[0],2)+ Math.pow(vector[1],2) + Math.pow(vector[2],2)) 
+ 	console.log(x + " " + this.hasExploded);
+ 	if (x < 0.25 && !this.hasExploded) {
+ 		console.log("PIÇAS");
+ 		this.hasExploded = true;
+ 		this.scene.explode(this.scene.targets[0].x,
+ 		  this.scene.targets[0].y, this.scene.targets[0].z);
+		this.scene.isExpanding = true;
+		
+ 	}
+ 	return x;
+ };
+
+ MyTorpedo.prototype.getRotationAngle = function(){
+
+ 	var vector = this.getVector();
+ 	var normals = Math.sqrt(vector[0]*vector[0]+vector[2]*vector[2])
+ 	var angle = Math.acos(vector[2]/normals);
+	
+ 	if(vector[0] >= 0)
+ 		return angle;
+ 	else
+ 		return 2*Math.PI-angle;
+ };
+
+ MyTorpedo.prototype.getDirection = function(){
+	
+	var hVect = [Math.sin(this.hRot), 0, Math.cos(this.hRot)];
+	var vVect = [0, Math.sin(-this.vRot), Math.cos(this.vRot)];
+
+	var total = [hVect[0]+vVect[0], hVect[1]+vVect[1], hVect[2]+vVect[2]];
+
+	var angle = Math.PI/2+this.vRot;
+	var direction = [
+		Math.sin(angle) * Math.sin(this.hRot),
+		Math.cos(angle),
+		Math.sin(angle) * Math.cos(this.hRot),
+	];
+
+	return direction;
+
+ };
+
+ MyTorpedo.prototype.readyToFire = function(){
+ 	
+ 	if(this.scene.targets.length == 0){
+ 		return "All targets eliminated.";
+ 	}
+	
+	
+ 	this.launch = true;
+ 	this.timePassed = 0;
+
+	
+	
+ 	var vector = this.getVector();
+ 	var distance = this.getTargetDistance();
+ 	this.firingTime = distance;
+ 	//var direction = this.getDirection();
+	
+	/*this.P2 = [
+		this.x + 2*Math.sin(this.initialHRot * degToRad),
+		this.y,
+ 		this.z + 2*Math.cos(this.initialHRot * degToRad),
+ 	]*/
+	
+	this.P2 = [
+	this.x + Math.sin(this.initialHRot * degToRad)/
+	  (Math.sqrt(1 + Math.pow(Math.sin(this.initialHRot * degToRad),2))), 
+	this.y + Math.sin(this.initialVRot * degToRad)/
+	  (Math.sqrt(1 + Math.pow(Math.sin(this.initialVRot * degToRad),2))), 
+	this.z + Math.cos(this.initialHRot * degToRad)/
+	   (Math.sqrt(1 + Math.pow(Math.sin(this.initialHRot* degToRad),2))), 
+	]
+
+	
+ 	this.P3 = [
+		this.scene.targets[0].x,
+		this.scene.targets[0].y +3,
+		this.scene.targets[0].z,
+ 	]
+
+
+ };
+
+ MyTorpedo.prototype.update = function(currTime){
+ 	
+ 	/*
+	this.lastime = this.lastime || currTime;
+ 	var dt = currTime - this.lastime;
+	this.lastime = currTime;
+	
+ 	if(this.rotation){
+		var totalRotation = this.getRotationAngle();
+
+ 		while(this.angle1 > totalRotation){
+ 			this.angle1 += totalRotation/10;
+ 		}
+
+ 		this.rotation = false;
+ 		this.bezier = true;
+ 		this.timePassed = 0;
+ 	}
+	*/
+ 
+	var totalRotation = this.getRotationAngle();
+
+ 	if(this.launch){
+		if(this.t <= 1.0){
+			
+			//this.orientation = (Math.PI/2-this.vRot)/(this.getTargetDistance*100);
+
+			var t = this.t;
+
+			var x1 = Math.pow((1-t), 3)*this.x + 3*t*Math.pow(1-t,2)*this.P2[0] + 3*Math.pow(t,2)*(1-t)*this.P3[0] + Math.pow(t,3)*this.P4[0];
+			
+			var y1 = Math.pow((1-t), 3)*this.y + 3*t*Math.pow(1-t,2)*this.P2[1] + 3*Math.pow(t,2)*(1-t)*this.P3[1] + Math.pow(t,3)*this.P4[1];
+			
+			var z1 = Math.pow((1-t), 3)*this.z + 3*t*Math.pow(1-t,2)*this.P2[2] + 3*Math.pow(t,2)*(1-t)*this.P3[2] + Math.pow(t,3)*this.P4[2];                             
+			
+			//this.hRot += totalRotation/11;
+			var dx = x1-this.x;
+			var dy = y1-this.y;
+			var dz = z1-this.z;
+
+			this.x = x1;
+			this.y = y1;
+			this.z = z1;
+
+			this.hRot = Math.atan(dx / dz) + (dz < 0 ? 180.0*degToRad : 0);
+			this.vRot = Math.atan(dy / Math.sqrt(dx*dx + dy*dy + dz*dz));
+
+
+			this.t += 1.0/(this.getTargetDistance() * 100);
+
+			this.y_offset = (1 - this.t) * -1.6;
+			if(this.y_offset > 0) this.y_offset = 0;
+			this.z_offset = (1 - this.t) * 1.5;
+			if(this.z_offset < 0) this.z_offset = 0;
+		
+		}
+		else{
+			this.scene.hit = true;
+			this.scene.launch = false;
+		}
+		
+		
+ 	}
+
+ 	this.timePassed = currTime;
+ };
+
+
+
+
+
+
